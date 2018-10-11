8b03b13e2211dedb68d9257270d3f82199abd6e4
Khalid Mohamed <kalmohammed56@gmail.com>
2018-10-11 13:41:53 +0300

    Item listing

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
-    logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
+	owner= models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+	name = models.CharField(max_length=120)
+	description = models.TextField()
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
+	logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
 
-    def __str__(self):
-    	return self.name
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		return reverse('restaurant-detail', kwargs={'restaurant_id':self.id})
+
+class Item(models.Model):
+	name=models.CharField(max_length=100)
+	description=models.TextField()
+	price=models.DecimalField(max_digits=10, decimal_places=3)
+	restaurant= models.ForeignKey(Restaurant, on_delete=models.CASCADE, default=1)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Restaurant
-from .forms import RestaurantForm, SignupForm, SigninForm
+from .models import Restaurant, Item
+from .forms import RestaurantForm, SignupForm, SigninForm, ItemForm
 from django.contrib.auth import login, authenticate, logout
 
 def signup(request):
@@ -50,8 +50,11 @@ def restaurant_list(request):
 
 
 def restaurant_detail(request, restaurant_id):
+    restaurant_obj=Restaurant.objects.get(id=restaurant_id)
+    items=Item.objects.filter(restaurant=restaurant_obj)
     context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
+        "restaurant": restaurant_obj,
+        "items":items,
     }
     return render(request, 'detail.html', context)
 
@@ -60,6 +63,8 @@ def restaurant_create(request):
     if request.method == "POST":
         form = RestaurantForm(request.POST, request.FILES)
         if form.is_valid():
+            form=form.save(commit=False)
+            form.owner = request.user
             form.save()
             return redirect('restaurant-list')
     context = {
@@ -67,10 +72,19 @@ def restaurant_create(request):
     }
     return render(request, 'create.html', context)
 
-def item_create(request):
-
+def item_create(request, restaurant_id):
+    form = ItemForm()
+    restaurant_obj=Restaurant.objects.get(id=restaurant_id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form=form.save(commit=False)
+            form.restaurant =restaurant_obj
+            form.save()
+            return redirect(restaurant_obj)
     context = {
-        
+        "form":form,
+        "restaurant":restaurant_obj,
     }
     return render(request, 'item_create.html', context)
 
